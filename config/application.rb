a2d26e028363e54da298f71d636dfa3adf8762ac
thealexcesar <thealexcesar@gmail.com>
2022-03-18 23:42:39 -0300

    init

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Webtoprint
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.time_zone = "Brasilia"
+    config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :"pt-BR"
   end
 end
